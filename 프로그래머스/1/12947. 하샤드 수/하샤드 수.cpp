29bc761eb82b392461db619f481f806bb315edcd
gyeommi <kkm417417@naver.com>
2026-02-04 16:04:42 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    bool answer = true;
+    int sum = 0, tmp = x;
+    
+    while (tmp) {
+        sum += tmp % 10;
+        tmp /= 10;
+    }
+    if (x % sum == 0) answer = true;
+    else answer = false;
+    return answer;
+}
